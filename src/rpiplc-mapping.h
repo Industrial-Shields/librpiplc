e6e6cdc1cc02ab336c3035e5b7223d9bad4213f3
Jordi Casals <jordi.casals@gmail.com>
2021-04-23 10:01:00 +0200

    Remap pins

--- src/rpiplc-mapping.h
+++ src/rpiplc-mapping.h
@@ -3,17 +3,14 @@
 
 #include <stdint.h>
 
-// TODO remap
-
-#define PIN_Q0_0 0x00004000
-#define PIN_Q0_0 0x00004000
-#define PIN_Q0_1 0x00004001
-#define PIN_Q0_2 0x00004002
-#define PIN_Q0_3 0x00004003
-#define PIN_Q0_4 0x00004004
-#define PIN_Q0_5 0x00004005
-#define PIN_Q0_6 0x00004006
-#define PIN_Q0_7 0x00004007
+#define PIN_Q0_0	0x00004015
+#define PIN_Q0_1	0x00004014
+#define PIN_Q0_2	0x00004013
+#define PIN_Q0_3	0x00004012
+#define PIN_Q0_4	0x00004011
+#define PIN_Q0_5	0x00004010
+#define PIN_Q0_6	0x00004001
+#define PIN_Q0_7	0x00004000
 
 static const uint32_t Q0_1 = PIN_Q0_1;
 static const uint32_t Q0_2 = PIN_Q0_2;
@@ -27,14 +24,14 @@ static const uint32_t A0_5 = PIN_Q0_5;
 static const uint32_t A0_6 = PIN_Q0_6;
 static const uint32_t A0_7 = PIN_Q0_7;
 
-#define PIN_Q1_0 0x00004008
-#define PIN_Q1_1 0x00004009
-#define PIN_Q1_2 0x0000400a
-#define PIN_Q1_3 0x0000400b
-#define PIN_Q1_4 0x0000400c
-#define PIN_Q1_5 0x0000400d
-#define PIN_Q1_6 0x0000400e
-#define PIN_Q1_7 0x0000400f
+#define PIN_Q1_0	0x00004002
+#define PIN_Q1_1	0x00004009
+#define PIN_Q1_2	0x00004006
+#define PIN_Q1_3	0x00004004
+#define PIN_Q1_4	0x00004007
+#define PIN_Q1_5	0x00004003
+#define PIN_Q1_6	0x00004005
+#define PIN_Q1_7	0x00004008
 
 static const uint32_t Q1_0 = PIN_Q1_0;
 static const uint32_t Q1_1 = PIN_Q1_1;
@@ -49,14 +46,14 @@ static const uint32_t A1_5 = PIN_Q1_5;
 static const uint32_t A1_6 = PIN_Q1_6;
 static const uint32_t A1_7 = PIN_Q1_7;
 
-#define PIN_Q2_0 0x00004100
-#define PIN_Q2_1 0x00004101
-#define PIN_Q2_2 0x00004102
-#define PIN_Q2_3 0x00004103
-#define PIN_Q2_4 0x00004104
-#define PIN_Q2_5 0x00004105
-#define PIN_Q2_6 0x00004106
-#define PIN_Q2_7 0x00004107
+#define PIN_Q2_0	0x00004106
+#define PIN_Q2_1	0x00004107
+#define PIN_Q2_2	0x00004105
+#define PIN_Q2_3	0x00004104
+#define PIN_Q2_4	0x00004103
+#define PIN_Q2_5	0x00004102
+#define PIN_Q2_6	0x00004101
+#define PIN_Q2_7	0x00004100
 
 static const uint32_t Q2_0 = PIN_Q2_0;
 static const uint32_t Q2_1 = PIN_Q2_1;
