17df6efdf238bac84c6b8b0dcb662b1809cfdcf2
Jordi Casals <jordi.casals@gmail.com>
2021-04-23 15:18:17 +0200

    Add value argument to analogWrite test

--- test/analogWrite.c
+++ test/analogWrite.c
@@ -8,6 +8,10 @@ int main(int argc, char* argv[]) {
 	int pin = A0_5;
 	int value = 1023;
 
+	if (argc >= 2) {
+		value = atoi(argv[2]);
+	}
+
 	printf("Set pin %08x to %d\n", pin, value);
 	analogWrite(pin, value);
 
