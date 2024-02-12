a39dd17087e59d302b9acbab8695bcca62e73c4e
JoanVC100 <jojovica2001@gmail.com>
2024-02-12 11:39:31 +0100

    When installing, also install test files

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ ROOT_DIR=$(dirname $(realpath "$0"))
 BUILD_DIR=$ROOT_DIR/build
 INSTALL_DIR=/tmp/librpiplc
 
-
 if [ "$#" -eq 3 ] && [ "$3" == "-d" ]; then
     DEBUG=-d
 fi
@@ -23,7 +22,9 @@ PLC_MODEL="$2"
 make -C "$BUILD_DIR" install
 
 if [ -d "$INSTALL_DIR" ]; then
-    sudo cp -r "$INSTALL_DIR"/* /usr/local/
+    sudo cp -r "$INSTALL_DIR/lib" /usr/local/
+    sudo cp -r "$INSTALL_DIR/include" /usr/local/
+    cp -r $INSTALL_DIR/test ~
 else
     echo "Could not locate installation files. Aborting..."
     exit 1
